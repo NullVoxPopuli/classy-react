c613a0a3497005a7339501cdc6807101c2fb8133
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-09 20:40:40 -0400

    Strict mode just works

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
