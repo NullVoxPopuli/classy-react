391533d81e41acf1ca740685ad1d18dbfe958ff4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-15 21:22:53 +0100

    with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,40 @@ import './App.css';
 import { useState, useHooks } from './classy-react';
 
 export default @useHooks class App {
-  @useState accessor count = 5;
+  @useState accessor id = 0;
+  @useState accessor json = {};
+
+  requestCharacter = async (id) => {
+    let response = await fetch(`https://swapi.dev/api/people/${id}`);
+    let json = await response.json();
+
+    return json;
+  }
+
+  handleClick = async () => {
+    this.json = await this.requestCharacter(this.id += 1);
+  }
 
   render = () => {
     return (
       <>
-        <div>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+        <div className="centered">
+          <div>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Classy React</h1>
+          <div className="card">
+            <button onClick={this.handleClick}>Get Next Character</button>
+          </div>
         </div>
-        <h1>Classy React</h1>
-        <div className="card">
-          <button onClick={() => this.count += 1}>count is {this.count}</button>
-        </div>
+        {this.json.name &&
+          <div>
+            {this.json.name}
+            <pre>{JSON.stringify(this.json, null, 2)}</pre>
+          </div>
+        }
       </>
     );
   }
